3e22c910f6a5ca03a11b8303369ab7866ba14f8a
crab8012 <crab8011@gmail.com>
2016-03-10 10:53:13 -0700

    Added an initialization/start script for PythonLabViewer

---
+++ PLVInit.py
@@ -0,0 +1,13 @@
+#Imports Go Here, Primarily for the main PythonLabViewer File.
+import platform
+#import PythonLabViewer as plv
+
+def Main():
+    print("Python Lab Viewer Initialization Program\n")
+    print("CRAB8012\n")
+    OperatingSystem = platform.system()
+    print(OperatingSystem)
+    #plv.Main(OperatingSystem)
+    
+    
+Main()
