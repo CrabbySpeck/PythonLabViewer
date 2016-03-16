41c67e7cda4cb4bcdd9414058a416f69dfed102d
crab8012 <crab8011@gmail.com>
2016-03-16 11:54:24 -0600

    Added part of a program to add python files to the program

---
+++ LabAdder.py
@@ -0,0 +1,5 @@
+def inputLabFiles(number):
+	i = 1
+	while i < number:
+		lab = input("Type the case-sensitive name for the Python Lab File, wihout the .py: ")
+		print("\n Now put the file in the 'LAB' folder\n")
